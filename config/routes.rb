eb55d92624dc0b28ec7f04facb57b3d40942d70b
Mamadgi-Aishwarya <64008227+Mamadgi-Aishwarya@users.noreply.github.com>
2020-10-30 14:59:08 +0530

    Bootstrap installed and styled questions index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :comments
   root "questions#index" 
   resources :users ,except: [:new]
   resources :questions 
+  resources :comments
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_094125) do
+ActiveRecord::Schema.define(version: 2020_10_30_054341) do
 
   create_table "comments", force: :cascade do |t|
     t.text "message"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2020_10_29_094125) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
-    t.integer "comment_id"
   end
 
   create_table "users", force: :cascade do |t|
