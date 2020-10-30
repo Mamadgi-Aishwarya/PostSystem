188ad4447b70573f8ddd8e4046d85ffade13b1cc
Mamadgi-Aishwarya <64008227+Mamadgi-Aishwarya@users.noreply.github.com>
2020-10-30 23:54:06 +0530

    Models dependent:destroy done

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
     belongs_to :user
-    has_many :comments
+    has_many :comments, dependent: :destroy
     validates :title, :description ,presence:true
     validates :title, uniqueness:true
     validates :description, length: { minimum: 20 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   #has_many :questions, dependent: :destroy
-  has_many :questions
-  has_many :comments
+  has_many :questions, dependent: :destroy
+  has_many :comments, dependent: :destroy
   validates :name, presence:true, uniqueness:true
   has_secure_password
 end
